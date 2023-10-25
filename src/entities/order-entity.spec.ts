8f6444a0e101ac7d0f353afb1ef51c1a0d3340b6
Maria Luiza <marialuiza.almeidafernandes@gmail.com>
2023-10-25 19:17:00 -0300

    fix: adjust tests

--- src/entities/order-entity.spec.ts
+++ src/entities/order-entity.spec.ts
@@ -1,16 +1,23 @@
 import { EOrderStatus } from "../shared/enums/order-status.enum";
 import { EOrderType } from "../shared/enums/order-type.enum";
+import { DomainRuleException } from "../shared/errors/domain-rule-exception";
 import { IOrder } from "../shared/interfaces/entities/order-entity.interface";
 import { IOrderItem } from "../shared/interfaces/entities/order-item-entity.interface";
-import { orderItens } from "../shared/mocks/orderItemMock";
 import { Order } from "./order.entity";
 
 describe("OrderEntity", () => {
-  const orderEntity = new Order();
-  Reflect.set(orderEntity, "items", orderItens);
-
   describe("[applyDiscount]", () => {
     it("should not apply discount at a promotional item", () => {
+      const orderEntity = new Order();
+
+      const orderItens: IOrderItem[] = [
+        { id: 1, isPromotional: true },
+        { id: 2, isPromotional: false },
+        { id: 3, isPromotional: false },
+      ] as IOrderItem[];
+
+      Reflect.set(orderEntity, "items", orderItens);
+
       expect(() => {
         orderEntity.applyDiscount({
           discount: 10,
@@ -20,6 +27,10 @@ describe("OrderEntity", () => {
     });
 
     it("should not apply discount at a online order", () => {
+      const orderEntity = new Order();
+
+      Reflect.set(orderEntity, "type", EOrderType.ONLINE);
+
       expect(() =>
         orderEntity.applyDiscount({
           discount: 10,
@@ -29,6 +40,10 @@ describe("OrderEntity", () => {
     });
 
     it("should not apply discount at a paid order", () => {
+      const orderEntity = new Order();
+
+      Reflect.set(orderEntity, "status", EOrderStatus.PAID);
+
       expect(() =>
         orderEntity.applyDiscount({
           discount: 10,
