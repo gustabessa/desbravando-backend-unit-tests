83497fc0921848c7a6d223cf45ce726dde50d6fc
Arthur Andrade <arthursandrade@icloud.com>
2023-10-25 18:42:56 -0300

    test: create test to order and order item entity

--- src/entities/order-item-entity.spec.ts
+++ src/entities/order-item-entity.spec.ts
@@ -1,4 +1,4 @@
-import { OrderItem } from "../../src/entities/order-item.entity";
+import { OrderItem } from "./order-item.entity";
 
 describe("OrderItemEntity", () => {
   describe("[updateOrderPrice]", () => {
@@ -6,7 +6,16 @@ describe("OrderItemEntity", () => {
       const orderItemEntity = new OrderItem();
       Reflect.set(orderItemEntity, "originalPrice", 10);
 
-      console.log({ orderItemEntity });
+      expect(()=> orderItemEntity.updateOrderPrice(8.9)).toThrowError();
+    });
+
+    it("should set new orderItem price if it is greater than 90% of the original price", () => {
+      const orderItemEntity = new OrderItem();
+      Reflect.set(orderItemEntity, "originalPrice", 10);
+
+      orderItemEntity.updateOrderPrice(15);
+
+      expect(orderItemEntity.orderPrice).toBe(15);
     });
   });
 });
