9e7c8e828b75ed78021dd566e9a265612151de97
geovanarochamp <geovana.rocha.martins@gmail.com>
2025-04-30 11:02:15 -0300

    test:add unit tests for formatDate function and handle invalid dates

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,3 +1,9 @@
 export function formatDate(date: Date) {
-  return `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()}`;
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+
+  return `${date.getUTCDate()}/${
+    date.getUTCMonth() + 1
+  }/${date.getUTCFullYear()}`;
 }
