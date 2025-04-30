42fb8fda733a17a4861fb2aed17fe78e06b0352b
Fabricio Lima <fabriciotrab2005@gmail.com>
2025-04-30 10:09:38 -0300

    test: fixing to use UTC in format date

--- src/utils/format-date.spec.ts
+++ src/utils/format-date.spec.ts
@@ -3,9 +3,9 @@ import { formatDate } from "./format-date";
 describe("format date function", () => {
     it("should return date in brasilian format", () => {
         const today = new Date(); 
-        const day = today.getDate();
-        const month = today.getMonth() + 1;
-        const year = today.getFullYear();
+        const day = today.getUTCDate();
+        const month = today.getUTCMonth() + 1;
+        const year = today.getUTCFullYear();
         const formattedDate = `${day}/${month}/${year}`;
 
         const result = formatDate(today);
@@ -15,9 +15,9 @@ describe("format date function", () => {
 
     it("should not return date in brasilian format with leading zero", () => {
         const today = new Date(2023, 0, 1); // January 1, 2023
-        const day = today.getDate();
-        const month = today.getMonth() + 1;
-        const year = today.getFullYear();
+        const day = today.getUTCDate();
+        const month = today.getUTCMonth() + 1;
+        const year = today.getUTCFullYear();
         const formattedDate = `0${day}/0${month}/${year}`;
 
         const result = formatDate(today);
